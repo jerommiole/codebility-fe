3b82c5fa534f5944023218340973d10206c2ba75
Andre Dan Dayaganon <your-email@example.com>
2023-12-06 23:47:25 +0800

    Conflict Fixed

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"jj1YY7jy1SwtGW8Jt06XcpHIEbIu/sK15Ainifrh4g4=\"\n}"
-=======
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"O1GI2JSxPmq0A2LCnI/Dm4x/2i3N4fsx2D1Y8umdxuA=\"\n}"
->>>>>>> 4721d006293813fbd5b51f069c92c54c0763ebea
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"HUZCoYEsjoSry+//l8x5cSPNCdAWpXztBg7WubKyDCk=\"\n}"
