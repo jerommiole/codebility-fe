ef5b306934bbd28b289e327cd8aac70d84a8bd89
David <david.estrelloso.tribugenia@gmail.com>
2023-12-07 14:49:47 +0800

    .next

--- .next/static/webpack/webpack.2541ea1e09051923.hot-update.js
+++ .next/static/webpack/webpack.2541ea1e09051923.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "35f1e591670775cb"; }
+/******/ 	__webpack_require__.h = function() { return "e5e89619324cd7c7"; }
 /******/ }();
 /******/ 
 /******/ }
