77c08174c48a7ff46a5e847eedc7be127cf3f7b2
David <david.estrelloso.tribugenia@gmail.com>
2023-12-07 14:26:25 +0800

    Final Sidebar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7be86ddb2c988ca1"; }
+/******/ 		__webpack_require__.h = function() { return "3143fb3116493bd0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
