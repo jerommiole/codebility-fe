ea9696ce56f1530add857036c76609c9d6002104
420ku <mordecai51213@gmail.com>
2023-12-07 18:38:29 +0800

    pull main

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: D:\XAAMP\htdocs\Codibility\codebility-fe\app\layout.tsx
+// File: /home/kopimi/Desktop/codebilityproject/client/codebility-fe/app/layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
