ae3102c1be2709ea51a54564dae416d2c8db0841
David <david.estrelloso.tribugenia@gmail.com>
2023-12-11 09:10:22 +0800

    .next

--- .next/server/server-reference-manifest.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"IIGin/nkj/4EjhVBSM0DchSr5OElUiU7NreYOOacLr0=\"\n}"
-=======
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"HJfRnvT2U7fYCh6CoWvoT9JkvMteQYyf6tlXsdA9jCg=\"\n}"
->>>>>>> dec248b980e56477f41f4045ec8f24c3b886ce76
