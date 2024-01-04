9a5c9f2832400419823ea615152b92f2954addad
Mark Ferdinan Aguilar <markferdinan.mfa@gmail.com>
2024-01-04 21:49:22 +0800

    added changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,15 @@ import Sidebar from "Components/sidebar"
 import Footer from "../Components/Footer/index"
 import ToggleMenu from "Components/toggleMenu"
 import WhyJoinUs from "./WhyJoinUs/page"
+import CoDevs from "./CoDevs/page";
 
 export default function Web() {
   return (
     <>
       <div className="flex h-screen w-full flex-col ">
         {/* LANDING PAGE */}
-        {/* <WhyJoinUs /> */}
+         <WhyJoinUs />
+          <CoDevs />
         <Footer />
       </div>
     </>
