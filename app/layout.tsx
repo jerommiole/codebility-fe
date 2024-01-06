f1902c10263c751aa6938d1768d1a8781e25f247
David <david.estrelloso.tribugenia@gmail.com>
2024-01-06 10:02:26 +0800

    dark

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import "styles/tailwind.css"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    // Remove dark to run normal mode
-    <html lang="en" className="">
+    // Remove dark for light mode
+    <html lang="en" className="dark">
       <body>{children}</body>
     </html>
   )
