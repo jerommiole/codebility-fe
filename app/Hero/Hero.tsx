89b03571686635f69718d0be2968f9c08aff3ca3
David <david.estrelloso.tribugenia@gmail.com>
2024-01-06 10:18:14 +0800

    Working

--- app/Hero/Hero.tsx
+++ app/Hero/Hero.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React from "react"
-import { Button } from "@/components/ui/button"
+import { Button } from "Components/ui/button"
 import Link from "next/link"
 import Image from "next/image"
 import Navbar from "./Navbar"
