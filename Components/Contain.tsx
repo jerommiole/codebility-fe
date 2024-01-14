fd21a4dd1247716086d74c2c7e40ee0b17b5ad5f
David <david.estrelloso.tribugenia@gmail.com>
2024-01-14 17:19:32 +0800

    Fixed Layout

--- Components/Contain.tsx
+++ Components/Contain.tsx
@@ -54,7 +54,9 @@ const Contain: React.FC<ContainProps> = ({ children }) => {
       {/* lg:overflow-hidden */}
       {/* ${!activeNav ? "overflow-y-auto" : "overflow-y-hidden"} */}
       <div className={`flex min-h-0 flex-1`}>
-        <div className={`flex-1 overflow-auto p-4 sm:pb-20`}>{children}</div>
+        <div className={`flex-1 ${!activeNav ? "overflow-y-auto" : "overflow-y-hidden"} p-0 sm:p-4 sm:pb-20`}>
+          {children}
+        </div>
       </div>
     </div>
   )
