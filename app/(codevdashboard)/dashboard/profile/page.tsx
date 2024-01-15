54b8de4bccd6026941285acdbce08f82455d6edf
Mark Ferdinan Aguilar <markferdinan.mfa@gmail.com>
2024-01-15 22:09:25 +0800

    feat: added image upload preview to profile settings

--- app/(codevdashboard)/dashboard/profile/page.tsx
+++ app/(codevdashboard)/dashboard/profile/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React from "react"
+import React, { useState } from "react"
 import Image from "next/image"
 
 import personIcon from "public/sampleProfile/settings/person.png"
@@ -8,57 +8,76 @@ import bellIcon from "public/sampleProfile/settings/bell.png"
 import gearIcon from "public/sampleProfile/settings/gear.png"
 import logIcon from "public/sampleProfile/settings/log.png"
 import infoIcon from "public/sampleProfile/settings/info.png"
-import { SvgBin, SvgCamera, SvgCircleAvatar, SvgEdit, SvgEdit2, SvgPlusCircleBlue } from "../../../../assets/icons"
+import {
+  SvgBin,
+  SvgCamera,
+  SvgCircleAvatar,
+  SvgEdit,
+  SvgEdit2,
+  SvgLock,
+  SvgPlusCircleBlue,
+} from "../../../../assets/icons"
 import Input from "../../../../Components/ui/forms/input"
 import UserInfoForm from "../../../../Components/profile/UserInfoForm"
 import { usePathname } from "next/navigation"
 import { cn } from "../../../../lib/utils"
 import ContactSocialsInfoForm from "../../../../Components/profile/ContactSocialsInfoForm"
 
+const profileMenuList = [
+  {
+    title: "Profile Info",
+    icon: personIcon,
+    href: "/profile",
+  },
+  {
+    title: "Change Password",
+    icon: <SvgLock />,
+    href: "/change-password",
+  },
+  {
+    title: "Notifications",
+    icon: bellIcon,
+    href: "/notifications",
+  },
+  {
+    title: "Settings",
+    icon: gearIcon,
+    href: "/settings",
+  },
+  {
+    title: "Activity Log",
+    icon: logIcon,
+    href: "/activity-log",
+  },
+  {
+    title: "About",
+    icon: infoIcon,
+    href: "/about",
+  },
+]
+
+const contactInfoList = {
+  phone: "+63901234123",
+  github: "dummy.github.io",
+  facebook: "https://www.facebook.com",
+  whatsApp: "https://www.facebook.com",
+  telegram: "https://www.facebook.com",
+  skype: "https://www.facebook.com",
+  linkedIn: "https://www.facebook.com",
+}
 const ProfilePage = () => {
   const pathname = usePathname()
 
-  const profileMenuList = [
-    {
-      title: "Profile Info",
-      icon: personIcon,
-      href: "/profile",
-    },
-    {
-      title: "Notifications",
-      icon: bellIcon,
-      href: "/notifications",
-    },
-    {
-      title: "Settings",
-      icon: gearIcon,
-      href: "/settings",
-    },
-    {
-      title: "Activity Log",
-      icon: logIcon,
-      href: "/activity-log",
-    },
-    {
-      title: "About",
-      icon: infoIcon,
-      href: "/about",
-    },
-  ]
+  const [isLoading, setIsLoading] = useState(false)
+  const [imageData, setImageData] = useState<string | null>(null)
+  const [file, setFile] = useState<string | null>("")
 
-  const contactInfoList = {
-    phone: "+63901234123",
-    github: "dummy.github.io",
-    facebook: "https://www.facebook.com",
-    whatsApp: "https://www.facebook.com",
-    telegram: "https://www.facebook.com",
-    skype: "https://www.facebook.com",
-    linkedIn: "https://www.facebook.com",
-  }
-
-  const OutlineButton = ({ children }: { children: React.ReactNode }) => {
+  const OutlineButton = ({ onClick, children }: { onClick: () => void; children: React.ReactNode }) => {
     return (
-      <div className="flex items-center gap-2.5 border border-gray03 px-[13px] py-[9px] text-xs text-secondaryColor">
+      <div
+        className="flex cursor-pointer items-center gap-2.5 border border-gray03 px-[13px] py-[9px] text-xs text-secondaryColor transition duration-300 hover:-translate-y-0.5"
+        onClick={onClick}
+      >
         {children}
       </div>
     )
@@ -72,33 +91,67 @@ const ProfilePage = () => {
     )
   }
 
+  const SettingMenu = () => {
+    return (
+      <ul className="flex flex-col gap-3 px-6">
+        {profileMenuList.map((profile) => {
+          const active = pathname === `/dashboard${profile.href}` ? "bg-gray02" : null
+          return (
+            <div
+              className={cn(
+                `flex w-full cursor-pointer items-center gap-10 rounded-sm px-4 py-2 hover:bg-gray02`,
+                `${active}`
+              )}
+              key={profile.title}
+            >
+              <div>
+                <Image src={profile.icon} alt={profile.title} width="14" height="14" />
+              </div>
+
+              <li className="text-left text-sm">{profile.title}</li>
+            </div>
+          )
+        })}
+        <div className="border-b">&nbsp;</div>
+      </ul>
+    )
+  }
+
+  const handleUploadAvatar = async (event: React.ChangeEvent<HTMLInputElement> | null | undefined) => {
+    // @ts-ignore
+    const file: any = event.target.files[0]
+    const allowedTypes = ["image/jpeg", "image/png", "image/gif"]
+
+    if (allowedTypes.includes(file.type)) {
+      const reader = new FileReader()
+
+      reader.onload = () => {
+        const image: any = reader.result
+        // @ts-ignore
+        const base64Image = image.split(",")[1]
+        setImageData(image)
+        // setValue("imageData", base64Image)
+        setFile(file.name)
+      }
+
+      reader.readAsDataURL(file)
+    } else {
+      console.log("The selected file is not an image in one of the supported formats.")
+    }
+  }
+
+  const handleRemoveAvatar = () => {
+    setImageData(null)
+    setFile(null)
+  }
+
   return (
     <div className="h-full p-[1.63rem]">
       <div className="flex h-full w-full">
         <div className="h-full w-[19.3125rem] border-r">
           <div className=" w-full p-2 py-3">
             <p className="mb-[2.5rem] pl-8 text-xl font-medium">CODEBILITY ACCOUNT</p>
-            <ul className="flex flex-col gap-3 px-6">
-              {profileMenuList.map((profile) => {
-                const active = pathname === `/dashboard${profile.href}` ? "bg-gray02" : null
-                return (
-                  <div
-                    className={cn(
-                      `flex w-full cursor-pointer items-center gap-10 rounded-sm px-4 py-2 hover:bg-gray02`,
-                      `${active}`
-                    )}
-                    key={profile.title}
-                  >
-                    <div>
-                      <Image src={profile.icon} alt={profile.title} width="14" height="14" />
-                    </div>
-
-                    <li className="text-left text-sm">{profile.title}</li>
-                  </div>
-                )
-              })}
-              <div className="border-b">&nbsp;</div>
-            </ul>
+            <SettingMenu />
           </div>
         </div>
         <div className="grow overflow-auto pl-5">
@@ -116,13 +169,19 @@ const ProfilePage = () => {
 
             <div className="pt-[39px]">
               <div className="flex space-x-14">
-                <div className="relative">
-                  <SvgCircleAvatar />
-                  <div className="absolute inset-0 h-full">
-                    <div className="flex h-full flex-col items-center justify-center">
-                      <SvgCamera />
-                      <p className="text-center text-[10px]">Upload Photo</p>
-                    </div>
+                <div className="relative h-[100px] w-[100px]">
+                  <div className="absolute inset-0">
+                    {imageData ? (
+                      <div className="flex h-full w-full items-center justify-center">
+                        <Image src={imageData} alt="avatar" className="rounded-full" fill />
+                      </div>
+                    ) : (
+                      <label className="flex h-full w-full cursor-pointer flex-col items-center justify-center rounded-full bg-gray03">
+                        <SvgCamera />
+                        <input type="file" className="hidden" onChange={handleUploadAvatar} />
+                        <p className="text-center text-[10px]">Upload Photo</p>
+                      </label>
+                    )}
                   </div>
                 </div>
                 <div className="flex flex-col justify-end gap-1">
@@ -131,11 +190,15 @@ const ProfilePage = () => {
                     dummy text of the printing and typesetting industry.{" "}
                   </p>
                   <div className="flex items-center gap-[8px]">
-                    <OutlineButton>
+                    <label
+                      className="flex cursor-pointer items-center gap-2.5 border border-gray03 px-[13px] py-[9px]
+                      text-xs text-secondaryColor transition duration-300 hover:-translate-y-0.5"
+                    >
                       <SvgEdit />
                       Change
-                    </OutlineButton>
-                    <OutlineButton>
+                      <Input type="file" className="hidden" onChange={handleUploadAvatar} />
+                    </label>
+                    <OutlineButton onClick={handleRemoveAvatar}>
                       <SvgBin />
                       Remove
                     </OutlineButton>
