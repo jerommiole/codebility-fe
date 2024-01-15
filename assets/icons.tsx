f66df14a21844b27ec3e300a2eedbb607ba3f4d8
Mark Ferdinan Aguilar <markferdinan.mfa@gmail.com>
2024-01-15 22:09:25 +0800

    feat: added image upload preview to profile settings

--- assets/icons.tsx
+++ assets/icons.tsx
@@ -79,3 +79,21 @@ export const SvgCheckCircle = (props: SVGProps<SVGSVGElement>) => (
     />
   </svg>
 )
+
+export const SvgLock = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth={1.5}
+    className="h-6 w-6"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
+    />
+  </svg>
+)
