09817cb6aa3150f9c5facc6a7781fec08ddcccc2
Mark Ferdinan Aguilar <markferdinan.mfa@gmail.com>
2024-01-18 03:03:30 +0800

    feat: :rocket: google signin

--- app/(codevdashboard)/template.tsx
+++ app/(codevdashboard)/template.tsx
@@ -2,10 +2,10 @@ import { SessionProvider } from "next-auth/react"
 import { getServerSession } from "next-auth"
 import { redirect } from "next/navigation"
 
-import { authOptions } from "../api/auth/[...nextauth]/route"
 import Sidebar from "../../Components/sidebar"
 import Contain from "../../Components/Contain"
 import React from "react"
+import { authOptions } from "../../lib/authOptions"
 
 export default async function Template({ children }: { children: React.ReactNode }) {
   const session = await getServerSession(authOptions)
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,5 @@
 import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-
-export const authOptions: any = {
-  secret: process.env.SECRET as string,
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-}
+import { authOptions } from "../../../../lib/authOptions"
 
 const handler = NextAuth(authOptions)
 
