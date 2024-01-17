3debec7ebb82d0466df90585caecdc0fa746ce49
Mark Ferdinan Aguilar <markferdinan.mfa@gmail.com>
2024-01-18 02:53:00 +0800

    feat: :rocket: google signin

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,12 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
-export const authOptions = {
-  secret: process.env.SECRET,
+export const authOptions: any = {
+  secret: process.env.SECRET as string,
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
 }
