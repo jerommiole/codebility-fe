70ad914ed05c6a320ece8dde62c28bbec48ce9e7
Mark Ferdinan Aguilar <markferdinan.mfa@gmail.com>
2024-01-21 21:04:17 +0800

    added changes

---
+++ lib/constants.ts
@@ -0,0 +1,8 @@
+const Url = process.env.NEXT_PUBLIC_BASE_API
+  ? `${process.env.NEXT_PUBLIC_BASE_API}/`
+  : "https://codebility-be.onrender.com/api/v1/production/"
+
+export const API = {
+  CODEVS: `${Url}users`,
+  AUTH_GOOGLE_CODEVS: `${Url}google/user`,
+}
