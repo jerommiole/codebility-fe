e4e79a479fd0d046491cdfc68afb6856829b373b
Mark Ferdinan Aguilar <markferdinan.mfa@gmail.com>
2024-01-23 15:17:51 +0800

    added changs

--- app/api/index.ts
+++ app/api/index.ts
@@ -9,3 +9,26 @@ export async function saveUserData(email: string, updatedData: any) {
     body: JSON.stringify(updatedData),
   })
 }
+
+export async function updateProfile(id: string, updatedData: any) {
+  const response = await fetch(`${API.AUTH_GOOGLE_CODEVS}/${id}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(updatedData),
+  })
+
+  return response.json()
+}
+
+export async function getCodev(email: any) {
+  const response = await fetch(`${API.AUTH_GOOGLE_CODEVS}/${email}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+
+  return response.json()
+}
