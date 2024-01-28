e9f3019b6c05139d1c59476c2677fb6c5788665c
David <david.estrelloso.tribugenia@gmail.com>
2024-01-28 16:01:08 +0800

    SignUp Modal Bug Fix

--- lib/validations/auth.ts
+++ lib/validations/auth.ts
@@ -8,21 +8,22 @@ export const SignUpValidation = z
     githubLink: z.string().min(1, { message: "Required" }),
     portfolioLink: z.string().min(1, { message: "Required" }),
     techstacks: z.string().min(1, { message: "Required" }),
-    password: z
-      .string()
-      .min(8, { message: "Password must be at least 8 characters long" })
-      .refine((value: string) => /[a-z]/.test(value), {
-        message: "Password must contain at least one lowercase letter",
-      })
-      .refine((value: string) => /[A-Z]/.test(value), {
-        message: "Password must contain at least one uppercase letter",
-      })
-      .refine((value: string) => /\d/.test(value), {
-        message: "Password must contain at least one digit",
-      })
-      .refine((value: string) => /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/.test(value), {
-        message: "Password must contain at least one special character",
-      }),
+    // password: z
+    //   .string()
+    //   .min(8, { message: "Password must be at least 8 characters long" })
+    //   .refine((value: string) => /[a-z]/.test(value), {
+    //     message: "Password must contain at least one lowercase letter",
+    //   })
+    //   .refine((value: string) => /[A-Z]/.test(value), {
+    //     message: "Password must contain at least one uppercase letter",
+    //   })
+    //   .refine((value: string) => /\d/.test(value), {
+    //     message: "Password must contain at least one digit",
+    //   })
+    //   .refine((value: string) => /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/.test(value), {
+    //     message: "Password must contain at least one special character",
+    //   }),
+    password: z.string().min(1, { message: "Required" }),
     confirmPassword: z.string().min(1, { message: "Required" }),
     schedule: z.string().min(1, { message: "Required" }),
     position: z.string().min(1, { message: "Required" }),
