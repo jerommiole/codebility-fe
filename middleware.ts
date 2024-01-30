7b4b04b1331ad3d86b401cd1c36a24aed75ad0f3
David <david.estrelloso.tribugenia@gmail.com>
2024-01-30 20:12:34 +0800

    Fixed build errors

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,5 @@
 export { default } from "next-auth/middleware"
 
-// matcher: ["/auth", "/api/:path*", "/dashboard/:path*", "/Interns/:path*", "/projects-completed/:path*"],
-
 export const config = {
   matcher: ["/((?!api|auth/signin|auth/signup|_next/static|.*\\..*|_next/image|$).*)"],
 }
