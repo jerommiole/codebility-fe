316b99b95b091b44adbde44abd79c9e29627cfc1
David <david.estrelloso.tribugenia@gmail.com>
2024-01-30 19:25:00 +0800

    Update Middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,7 @@
 export { default } from "next-auth/middleware"
 
+// matcher: ["/auth", "/api/:path*", "/dashboard/:path*", "/Interns/:path*", "/projects-completed/:path*"],
+
 export const config = {
-  matcher: ["/auth", "/api/:path*", "/dashboard/:path*", "/Interns/:path*", "/projects-completed/:path*"],
+  matcher: ["/((?!api|auth/signin|auth/signup|_next/static|.*\\..*|_next/image|$).*)"],
 }
