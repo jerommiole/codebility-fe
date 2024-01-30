681698ea842468d2726e53a6ecf410b279e32e26
David <david.estrelloso.tribugenia@gmail.com>
2024-01-30 19:41:23 +0800

    Adjust sidebar

--- app/(codevdashboard)/template.tsx
+++ app/(codevdashboard)/template.tsx
@@ -10,39 +10,7 @@ import { useRouter } from "next/navigation"
 
 export default function Template({ children }: { children: React.ReactNode }) {
   const session = useSession()
-  console.log(session)
   const router = useRouter()
-  // const getUser = async () => {
-  //   try {
-  //     const url = `${process.env.NEXT_PUBLIC_BASE_API}/auth/login/success`
-
-  //     fetch(url, {
-  //       method: "GET",
-  //       credentials: "include",
-  //     })
-  //       .then((data) => {
-  //         console.log(data)
-  //       })
-  //       .catch((error) => {
-  //         console.log(error)
-  //       })
-  //   } catch (err) {
-  //     console.log(err)
-  //   }
-  // }
-  // getUser()
-  // console.log("rendering...")
-
-  //Uncomment this for securing the codevdashboard
-
-  // useEffect(() => {
-  //   if (session?.status === "unauthenticated") {
-  //     router.push("/signin")
-  //   }
-  // }, [router, session?.status])
-
-  // Uncomment this to enable loading screen
-
   if (session?.status === "loading")
     return (
       <div className="fixed left-0 top-0 z-20 flex h-screen w-screen flex-col items-center justify-center gap-10 bg-black">
@@ -53,10 +21,11 @@ export default function Template({ children }: { children: React.ReactNode }) {
         </div>
       </div>
     )
-  return (
-    <>
-      <Sidebar />
-      <Contain>{children}</Contain>
-    </>
-  )
+  if (session?.status === "authenticated")
+    return (
+      <>
+        <Sidebar />
+        <Contain>{children}</Contain>
+      </>
+    )
 }
