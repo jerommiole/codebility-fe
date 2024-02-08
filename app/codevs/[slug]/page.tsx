b343a7f3d8d74a59f0957f6b8c7d4e1fe1695b95
unknown <nerwinalamas@gmail.com>
2024-02-08 12:36:44 +0800

    fix filter and dark mode compatibility just incase

--- app/codevs/[slug]/page.tsx
+++ app/codevs/[slug]/page.tsx
@@ -6,10 +6,41 @@ import Image from "next/image"
 import { skillList } from "../../../lib/statisData"
 import axios from "axios"
 
+interface workExperience {
+  company: string;
+  date: string;
+  id: string;
+  position: string;
+  profileId: string;
+  short_desc: string;
+  userWorkExpId: string;
+}
+
+interface User {
+  id: string;
+  name: string;
+  short_bio?: string;
+  image_icon?: string;
+  tech_stacks?: string[];
+  addtl_skills?: string[];
+  position: string;
+  work_experience?: workExperience[];
+  UserProjects?: any[];
+  clients?: any[];
+}
+
+// NOTES: WALA PA SA API FOR RESUME PAGE
+// EDUCATION
+// WORK EXPERIENCES
+//    -START DATE
+//    -END DATE
+//    -TASKS[] NOT SHORT DESC
+//    -TECH USED []
+
 const Page = ({ params }: { params: { slug: string } }) => {
   const id = params.slug
 
-  const [data, setData] = useState<any[]>([])
+  const [data, setData] = useState<User | null>(null)
   const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
@@ -31,6 +62,8 @@ const Page = ({ params }: { params: { slug: string } }) => {
     fetchCoDevsData(id)
   }, [id])
 
+  // console.log("eto", data);
+
   if (isLoading) {
     return <div className="bg-black">Loading....</div>
   }
@@ -49,9 +82,6 @@ const Page = ({ params }: { params: { slug: string } }) => {
     },
     {
       name: "css",
-    },
-    {
-      name: "github",
     },
     {
       name: "firebase",
@@ -104,37 +134,36 @@ const Page = ({ params }: { params: { slug: string } }) => {
 
   return (
     <>
-      <section className="mx-auto max-w-5xl px-4 text-gray01 md:container ">
+      <section className="mx-auto max-w-5xl px-4 dark:text-gray01 md:container">
         <div className="mb-8 grid gap-y-5 pt-8 lg:h-[20rem] lg:grid-cols-6 lg:grid-rows-3 lg:gap-y-0 lg:relative">
-          <div className="border-b border-b-tealColor pb-3 lg:col-span-2 lg:pt-0">
+          <div className="border-b dark:border-b-tealColor pb-3 lg:col-span-2 lg:pt-0">
             <div className="flex justify-between">
-              <h2 className="mb-3 text-2xl font-semibold text-white md:text-5xl">John Doe</h2>
+              <h2 className="mb-3 text-2xl font-semibold dark:text-white md:text-5xl">{data?.name}</h2>
               <Image src="/download.svg" alt="Download Button" width={25} height={25} className="lg:hidden" />
             </div>
             <p className="max-w-60 text-base font-normal lg:text-xl">Frontend Developer</p>
             <span className="w-full"></span>
           </div>
           <div className="flex w-full items-center justify-center lg:row-span-2">
-            <div className="flex h-full w-[10.6875rem] items-center bg-indigo-950 pt-3 lg:h-[20rem] lg:pt-0">
-              <Image src="/sampleProfile/profile.png" width="328" height="390" alt="profile" />
+            <div className="flex h-full w-[10.6875rem] items-center bg-slate-100 dark:bg-indigo-950 pt-3 lg:h-[20rem] lg:pt-0">
+              <Image src={data?.image_icon ?? "/sampleProfile/profile.png"} width="328" height="390" alt="profile" />
             </div>
           </div>
           <div className="lg:col-span-2 lg:row-start-2 lg:mt-10">
             <div>
-              <p className="text-base text-tealColor">About Me</p>
+              <p className="text-base dark:text-tealColor">About Me</p>
               <p className="pl-4 pt-4 text-xs">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                industry's standard dummy text ever since the 1500s,
+                {data?.short_bio}
               </p>
             </div>
           </div>
           <Image src="/download.svg" alt="Download Button" width={40} height={40} className="hidden lg:flex lg:absolute lg:right-5 lg:top-5 cursor-pointer" />
         </div>
           
-        <p className="hidden text-center text-base text-tealColor lg:block">Work Experience</p>
+        <p className="hidden text-center text-base dark:text-tealColor lg:block">Work Experience</p>
         <section className="lg:grid-rows-0 mb-5 grid grid-cols-1 lg:grid-cols-6">
           <div className="lg:col-span-2 lg:row-start-2 lg:pb-14">
-            <p className="text-base text-tealColor">Education</p>
+            <p className="text-base dark:text-tealColor">Education</p>
             <div className="pb-6 pl-4 pt-5 text-xs">
               <ul className="flex w-full flex-col gap-3">
                 {educList.map((educ, i) => (
@@ -148,16 +177,17 @@ const Page = ({ params }: { params: { slug: string } }) => {
             </div>
           </div>
           <div className="lg:col-span-2 lg:row-start-3 lg:pb-14">
-            <p className="text-base text-tealColor">Skills</p>
+            <p className="text-base dark:text-tealColor">Skills</p>
             <div className="pb-6 pl-4 pt-5 text-xs">
               <ul className="flex w-full items-center gap-3">
-                {skillData.map((skill, i) => (
-                  <li key={`skill-${i}`}>
+                {data?.tech_stacks && data.tech_stacks?.map((skill, i) => (
+                  <li key={`skill-${i}`} className="w-5">
                     <Image
-                      src={skillList[skill?.name]?.icon!}
-                      alt={skill.name}
-                      width={20}
-                      height={20}
+                      src={`/techStack/${skill.toLowerCase()}.png`}
+                      alt={`${skill} logo`}
+                      width={1000}
+                      quality={100}
+                      height={1000}
                       className=" transition duration-300 hover:-translate-y-0.5"
                     />
                   </li>
@@ -169,22 +199,28 @@ const Page = ({ params }: { params: { slug: string } }) => {
           <div className="row-span-10 mt-10 lg:col-span-3">
             <p className="pb-5 text-center text-base text-tealColor lg:hidden lg:pb-0">Work Experience</p>
             <div className="flex flex-col">
+              {/* {data?.work_experience && data?.work_experience.map((item, i) => ( */}
               {workExperienceData.map((item, i) => (
-                <div key={`skill-${i}`} className="relative border-l border-tealColor pb-6 pl-10">
+                <div key={`skill-${i}`} className="relative border-l dark:border-tealColor pb-6 pl-10">
                   <p className="mb-1 text-xs text-secondaryColor lg:text-sm">
+                    {/* WALA PA SA API */}
                     {item.fromYear} - {item.toYear}
+                    {/* {item.date} */}
                   </p>
-                  <p className="mb-2 text-lg font-bold text-gray01 lg:text-xl">{item.position}</p>
-                  <p className="mb-2 text-sm text-gray01 lg:text-lg">{item.company}</p>
+                  <p className="mb-2 text-lg font-bold dark:text-gray01 lg:text-xl">{item.position}</p>
+                  <p className="mb-2 text-sm dark:text-gray01 lg:text-lg">{item.company}</p>
                   <ul className="pl-4">
-                    {item.tasks?.map((task: any, i) => (
+                    {/* {item.short_desc} */}
+                    {/* WALA PA SA API */}
+                    {item.tasks?.map((task: any, i: any) => (
                       <li className="list-disc text-xs lg:text-sm" key={i}>
                         {task}
                       </li>
                     ))}
                   </ul>
                   <div className="m-2 flex gap-2">
-                    {item.technologies?.map((task: any, i) => (
+                    {/* WALA PA SA API */}
+                    {item.technologies?.map((task: any, i: any) => (
                       <Image
                         src={`/techStack/${task}.png`}
                         alt={task + " icon"}
@@ -207,6 +243,7 @@ const Page = ({ params }: { params: { slug: string } }) => {
         <div>
           <p className=" text-base">CODEBILITY PROJECTS</p>
           <div className="mt-7 flex flex-col gap-y-10 lg:gap-y-5">
+            {/* data.clients */}
             {[1, 2, 3].map((i, j) => (
               <div key={`div-${j}`} className="flex flex-col items-center justify-center gap-2 lg:flex-row lg:gap-10">
                 <div className="gap-3 lg:flex hover:cursor-pointer">
@@ -268,6 +305,7 @@ const Page = ({ params }: { params: { slug: string } }) => {
         <div>
           <p className=" text-base">PERSONAL PROJECTS</p>
           <div className="mt-7 flex flex-col gap-y-10 lg:gap-y-5">
+            {/* data.UserProjects */}
             {[1, 2, 3].map((i, j) => (
               <div key={`div-${j}`} className="flex flex-col items-center justify-center gap-2 lg:flex-row lg:gap-10">
                 <div className="gap-3 lg:flex hover:cursor-pointer">
