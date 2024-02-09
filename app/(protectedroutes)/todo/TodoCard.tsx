b22bae89e8436c46a769e7987d6d8babf31fbd37
Jahz10 <bullymedont.justify@gmail.com>
2024-02-09 11:54:10 +0800

    position pagination to bottom right

--- app/(protectedroutes)/todo/TodoCard.tsx
+++ app/(protectedroutes)/todo/TodoCard.tsx
@@ -5,7 +5,7 @@ import { SvgBox, SvgPin, SvgMore, SvgWarning } from "assets/icons"
 
 const TodoCard = () => {
   return (
-    <Card className="mx-auto h-[240px] w-full max-w-[400px] p-3 ">
+    <Card className="mx-auto h-[250px] w-full max-w-[400px] p-3 ">
       <div className="flex justify-between">
         <div className="flex w-[60px] items-center justify-between">
           <SvgBox className="cursor-pointer" />
@@ -22,7 +22,9 @@ const TodoCard = () => {
           <h1 className="font-600 text-xl text-[#02FFE2]">JIRA TICKET</h1>
           <p className="text-xs text-gray-500">11/28/2023</p>
         </div>
-        <p className="text-xs text-[#9747FF] sm:text-sm">Git Link : https://github.com/mitzukin/AnyStore</p>
+        <p className="text-xs text-[#9747FF] sm:text-sm md:text-xs lg:text-sm">
+          Git Link : https://github.com/mitzukin/AnyStore
+        </p>
         <div className="p-3">
           <p className="text-sm text-gray-500">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde accusantium ipsa voluptatibus, ut ipsum minima
@@ -31,43 +33,43 @@ const TodoCard = () => {
         </div>
       </div>
       {/* Card Footer  */}
-      <div className="flex items-center justify-between">
+      <div className="mt-2 flex items-center justify-between">
         {/* left  container */}
         <div className="flex gap-2">
-          <div className="flex items-center gap-3 rounded-full border px-2 py-1 text-xs sm:px-3">
+          <button className="flex cursor-pointer items-center gap-3 rounded-full border px-2 py-1 text-xs sm:px-3">
             <div className="h-full w-0.5 bg-[#9747FF]" />
             <p>Codebility</p>
-          </div>
-          <div className="flex items-center gap-3 rounded-full border px-2 py-1 text-xs sm:px-3">
+          </button>
+          <button className="flex cursor-pointer items-center gap-3 rounded-full border px-2 py-1 text-xs sm:px-3">
             <div className="h-full w-0.5 bg-[#9747FF]" />
             <p>Z-Form</p>
-          </div>
+          </button>
         </div>
         {/* right container  */}
         <div className="">
           <p className="mb-1 text-xs">Team</p>
           <div className="flex items-center gap-1">
-            <div className="h-5 w-5 rounded-full bg-[#2f85a1] sm:h-7 sm:w-7">
+            <div className="h-7 w-7 rounded-full bg-[#2f85a1]">
               <Image
-                className="h-full w-full rounded-full object-cover"
+                className="h-full w-full cursor-pointer rounded-full object-cover"
                 src="/image25.png"
                 width={35}
                 height={5}
                 alt="team-member"
               />
             </div>
-            <div className="h-5 w-5 rounded-full bg-[#34cc80] sm:h-7 sm:w-7">
+            <div className="h-7 w-7 rounded-full bg-[#34cc80]">
               <Image
-                className="h-full w-full rounded-full object-cover"
+                className="h-full w-full cursor-pointer rounded-full object-cover"
                 src="/image32.png"
                 width={35}
                 height={5}
                 alt="team-member"
               />
             </div>
-            <div className="h-5 w-5 rounded-full bg-[#9747FF] sm:h-7 sm:w-7">
+            <div className="h-7 w-7 rounded-full bg-[#9747FF]">
               <Image
-                className="h-full w-full rounded-full object-cover"
+                className="h-full w-full cursor-pointer rounded-full object-cover"
                 src="/image33.png"
                 width={35}
                 height={5}
--- app/(protectedroutes)/todo/page.tsx
+++ app/(protectedroutes)/todo/page.tsx
@@ -17,34 +17,34 @@ import { Card } from "Components/ui/card"
 
 const TodoPage = () => {
   return (
-    <div className="flex h-full flex-col justify-between ">
+    <div className="relative flex h-full flex-col justify-between ">
       <div className="my-4 flex items-center justify-between px-1 sm:px-10 md:px-0">
         <h1 className="font-semibold sm:text-xl md:text-2xl">TO DO LIST</h1>
         <div className="flex items-center gap-4 text-xs sm:text-base">
           <div className="flex items-center gap-2">
-            Add <AddSVG className="text-text4 cursor-pointer" />
+            Add <AddSVG className="cursor-pointer text-text4" />
           </div>
           <div className="flex items-center gap-2">
-            Filter <FilterSVG className="text-text4 cursor-pointer" />
+            Filter <FilterSVG className="cursor-pointer text-text4" />
           </div>
           <div className="flex items-center gap-2">
-            Priority <SvgPriority className="cursor-pointer"/>
+            Priority <SvgPriority className="cursor-pointer" />
           </div>
         </div>
       </div>
-      <div className="grid place-items-start gap-2 overflow-y-scroll md:grid-cols-2 xl:grid-cols-3">
+      <div className="mb-4 grid place-items-start gap-2 overflow-y-scroll md:grid-cols-2 xl:grid-cols-3">
         {"qwert".split("").map((i) => (
           // Add key prop
           <TodoCard key={i} />
         ))}
-        <Card className="mx-auto flex h-[240px] w-full max-w-[400px] items-center justify-center cursor-pointer">
+        <Card className="mx-auto flex h-[250px] w-full max-w-[400px] cursor-pointer items-center justify-center">
           <div className="flex items-center gap-2">
-            <AddSVG className="text-text4 cursor-pointer" />
+            <AddSVG className="cursor-pointer text-text4" />
             ADD NEW TASK
           </div>
         </Card>
       </div>
-      <Pagination className="mt-4">
+      <Pagination className="right-0 mt-4 w-[350px] md:absolute md:-bottom-14 xl:-bottom-8">
         <PaginationContent>
           <PaginationItem>
             <PaginationPrevious href="#" />
