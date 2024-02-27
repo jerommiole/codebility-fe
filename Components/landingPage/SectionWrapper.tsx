ab7a195c1925fdfee64acf5c4d044fb64e060095
nayrx <espineda.ryanmiguel.c200571@gmail.com>
2024-02-27 13:42:37 +0800

    added index.js to refactor imports

--- Components/landingPage/SectionWrapper.tsx
+++ Components/landingPage/SectionWrapper.tsx
@@ -1,11 +1,12 @@
 import React, { ReactNode } from "react"
 
 interface SectionProps {
-  children: ReactNode
+  children: ReactNode,
+  style?: object
 }
 
-const SectionWrapper: React.FC<SectionProps> = ({ children }) => {
-  return <section className="mx-auto max-w-[1400px] px-5 py-5 md:p-16 lg:p-20 lg:py-40">{children}</section>
+const SectionWrapper: React.FC<SectionProps> = ({ children, style }) => {
+  return <section className="mx-auto max-w-[1400px] px-5 py-5 md:p-16 lg:p-20 lg:py-40" style={style}>{children}</section>
 }
 
 export default SectionWrapper
