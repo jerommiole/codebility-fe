da4169a03868a5d8c139f5a890786b3ca811f58d
jerommiole <miolejerom@gmail.com>
2024-02-29 20:17:40 +1300

    fixing: removing toggleMenu

--- Components/shared/dashboard/Navbar.tsx
+++ Components/shared/dashboard/Navbar.tsx
@@ -4,15 +4,11 @@ import Image from "next/image"
 import React from "react"
 
 import avatar from "@/public/assets/images/default-avatar-male.png"
-import useGoogleAuthCookie from "hooks/use-cookie"
-import ToggleMenu from "@/Components/toggleMenu"
 import Link from "next/link"
 import Theme from "./Theme"
 import MobileNav from "./MobileNav"
 
 const Navbar = () => {
-  const gooogleAuth = useGoogleAuthCookie()
-
   return (
     <>
       <nav className="background-light900_dark200 fixed top-0 z-10 w-full border-b">
