83a7dece292eb580c5748f39a39b8a82eef0e53a
jerommiole <miolejerom@gmail.com>
2024-03-01 01:09:54 +1300

    add: tweak calendar

--- app/(protectedroutes)/dashboard/page.tsx
+++ app/(protectedroutes)/dashboard/page.tsx
@@ -6,9 +6,8 @@ import H1 from "@/Components/shared/dashboard/H1"
 
 export default function DashboardPage() {
   return (
-    <div className="flex flex-col gap-6">
+    <div className="flex flex-col gap-4">
       <H1>Dashboard</H1>
-
       <div className="flex flex-col gap-6 md:flex-row">
         <div className="md:basis-[60%]">
           <div className="flex flex-col gap-6">
