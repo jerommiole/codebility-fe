431957e9e6b03cfa96cc00f601951d60483a14aa
Jahz10 <bullymedont.justify@gmail.com>
2024-03-06 14:21:27 +0800

    fix codevs card mobile column

--- app/(landingPage)/codevs/CodevsCard.tsx
+++ app/(landingPage)/codevs/CodevsCard.tsx
@@ -11,9 +11,9 @@ import { IconGithub } from "@/public/assets/svgs"
 const CodevsCard = ({ users }: { users: any }) => {
   return (
     <div className="m-auto w-full max-w-5xl">
-      <div className="flex flex-wrap justify-between gap-2">
+      <div className="grid place-items-center gap-2 sm:grid-cols-2 sm:place-items-start md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
         {users.map((user: any, index: number) => (
-          <div key={user.id} className="w-full max-w-[180px]">
+          <div key={user.id} className="mx-auto w-full max-w-[200px]">
             <div className="flex flex-col items-center gap-2 rounded-lg p-4 text-center lg:bg-transparent">
               <Link href={`/codevs/${user.id}`}>
                 <Image
