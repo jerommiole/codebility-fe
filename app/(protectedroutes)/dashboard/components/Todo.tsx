11c60f9ca8d050117572a6e421412dd74029c32b
Jahz10 <bullymedont.justify@gmail.com>
2024-03-07 23:26:52 +0800

    todo list api integration

--- app/(protectedroutes)/dashboard/components/Todo.tsx
+++ app/(protectedroutes)/dashboard/components/Todo.tsx
@@ -2,14 +2,30 @@
 
 import { useState } from "react"
 
-type TodoProps = {
-  title: string
-  desc: string
-  duration?: string
-  id: number
+interface Tag {
+  tag: string
 }
 
-const Todo = ({ title, desc, duration, id }: TodoProps) => {
+interface TodoProps {
+  id: string
+  title: string
+  subheader: string
+  full_description: string
+  userTodoId: string | null
+  projectId: string | null
+  prio_level: string
+  github_link: string
+  created_at: string
+  updated_at: string
+  tagId: string[]
+  tags: Tag[]
+  userTodo: any // Define type accordingly
+  projects: any // Define type accordingly
+}
+
+const Todo = ({ todo }: { todo: TodoProps }) => {
+  const { title, subheader, full_description, id } = todo // Destructuring todo object
+
   const [check, setCheck] = useState<boolean>(false)
 
   return (
@@ -22,9 +38,9 @@ const Todo = ({ title, desc, duration, id }: TodoProps) => {
           >
             <div className={`${check ? "bg-teal" : ""} h-full w-full`}></div>
           </div>
-          <div className={`flex flex-col justify-center ${duration ? "gap-0" : ""}`}>
+          <div className={`flex flex-col justify-center ${subheader ? "gap-0" : ""}`}>
             <div className="text-blue-100">{title}</div>
-            <div className="text-xs">{duration}</div>
+            <div className="text-xs">{subheader}</div> {/* Changed desc to subheader */}
           </div>
         </div>
         <div className="text-md cursor-pointer text-violet">View</div>
