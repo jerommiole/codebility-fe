68ecc2fc2075688646aa8fb17f7e089404481dea
itsmeadel <adelainediola@gmail.com>
2024-03-07 20:12:52 +0300

    Created structural table

--- app/(protectedroutes)/time-tracker/TimeTrackerTable.tsx
+++ app/(protectedroutes)/time-tracker/TimeTrackerTable.tsx
@@ -1,7 +1,41 @@
 import React from "react"
+import Box from "@/Components/shared/dashboard/Box"
+import H1 from "@/Components/shared/dashboard/H1"
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/Components/ui/table"
 
 const TimeTrackerTable = () => {
-  return <div>TimeTrackerTable</div>
+  return (
+    <Box>
+      <H1>Time Log  </H1>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Select</TableHead>
+            <TableHead>Date</TableHead>
+            <TableHead>Task</TableHead>
+            <TableHead>Time</TableHead>
+            <TableHead>Time Total</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {Array(10)
+            .fill(null)
+            .map((value, index) => (
+              <TableRow>
+                <TableCell>
+                  <input type="checkbox" />
+                </TableCell>
+                <TableCell>11/28/23</TableCell>
+                <TableCell>Jira Ticket</TableCell>
+                <TableCell>12:34 P - 00:06 P</TableCell>
+                <TableCell>00H 00m</TableCell>
+                <TableCell>Edit</TableCell>
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+    </Box>
+  )
 }
 
 export default TimeTrackerTable
