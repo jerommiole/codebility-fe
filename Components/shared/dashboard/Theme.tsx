cf83d8262ee7fc101de026ffe3a9eef79393dd17
itsmeadel <adelainediola@gmail.com>
2024-03-11 00:01:38 +0300

    toggle switch  modifed

--- Components/shared/dashboard/Theme.tsx
+++ Components/shared/dashboard/Theme.tsx
@@ -9,6 +9,12 @@ import { themes } from "@/constants"
 const Theme = () => {
   const { mode, setMode } = useTheme()
 
+  const toggleTheme = () => {
+    const newMode = mode === "light" ? "dark" : "light"
+    setMode(newMode)
+    localStorage.theme = newMode
+  }
+
   return (
     <>
       {/* <div>
@@ -18,7 +24,7 @@ const Theme = () => {
           <Image src="/assets/svgs/icon-moon.svg" alt="Moon" width={20} height={20} className="active-theme" />
         )}
       </div> */}
-      <div className="flex gap-4">
+      {/*<div className="flex gap-4">
         {themes.map((item) => (
           <div
             key={item.value}
@@ -42,13 +48,32 @@ const Theme = () => {
             />
             <p
               className={`body-semibold text-light-500 ${
-                mode === item.value ? "text-primary" : "text-dark100_light900"
-              }`}
+                mode === item.value ? "text-primary-500" : "text-dark100_light900"
+              }`} 
             >
-              {item.label}
+              {item.label}  
             </p>
           </div>
-        ))}
+        ))} */}
+
+      <div className="flex gap-4">
+        <div onClick={toggleTheme} className="flex cursor-pointer items-center gap-4 py-2 focus:bg-light-800 dark:focus:bg-dark-400">
+          <Image
+            src={`/assets/svgs/${mode === "light" ? "icon-moon.svg" : "icon-sun.svg"}`}
+            alt={mode === "light" ? "Moon" : "Sun"}
+            width={20}
+            height={20}
+            className="active"
+            style={{ filter: mode === 'light' ? 'invert(1)' : 'none' }}
+          />
+          <p
+            className={`body-semibold text-light-500 ${
+              mode === "light" ? "text-primary-500" : "text-dark100_light900"
+            }`}
+          >
+          
+          </p>
+        </div>
       </div>
     </>
   )
